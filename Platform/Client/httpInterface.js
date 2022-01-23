64b2baa58fbbce39cd3d8f1b2b9650ed065f96c5
Ira Miller <public@iramiller.com>
2022-01-23 16:41:42 -0500

    plugin utils mapped

--- Platform/Client/httpInterface.js
+++ Platform/Client/httpInterface.js
@@ -1080,7 +1080,8 @@ exports.newHttpInterface = function newHttpInterface() {
                                         // If upstream has not been set. Set it now
                                         console.log(isUpstreamSet)
                                         if (isUpstreamSet === false) {
-                                            await git.addRemote('upstream', `https://github.com/Superalgos/${repo}`).catch(errorResp);
+                                            console.log(`https://github.com/Superalgos/${global.env.PROJECT_PLUGIN_MAP[repo].repo}`)
+                                            await git.addRemote('upstream', `https://github.com/Superalgos/${global.env.PROJECT_PLUGIN_MAP[repo].repo}`).catch(errorResp);
                                         }
                                         console.log(currentBranch)
                                         // Pull branch from main repo
